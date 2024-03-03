871fa192171296ae2beb0212fbc534d43e8cf477
arturogood17 <arturo_good17@hotmail.com>
2024-03-03 03:26:24 +0100

    Sorting the list

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def main():
     letters = num_letter(text)
     print(f"--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
+    print("")
+    new_l(letters)
+    print("")
+    print("--- End report ---")
 
 
 
@@ -27,8 +31,18 @@ def num_letter(text):
     return dicc_letters
 
 def sort_on(letters):
-
-    
-
+    for i in letters:
+        return letters[i]
+        
+def new_l(letters):
+    l_sort= []
+    for i in letters:
+        if i.isalpha():
+           new_dict = {i:letters[i]}
+           l_sort.append(new_dict)
+    l_sort.sort(reverse=True, key=sort_on)
+    for item in l_sort:
+        for key in item:
+            print (f"The {item} character was found {item[key]} times")
 
 main()
