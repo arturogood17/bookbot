f0979c58a843a7552b658c38552bca97a870e876
arturogood17 <arturo_good17@hotmail.com>
2025-11-14 18:56:55 +0100

    change: Now main prints the whole book with the help of get_book_text()

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+from functions import *
+
 def main():
-    print("greetings boots")
+    abs_path = os.path.dirname(__file__)
+    relative_path = "books/frankenstein.txt"
+    book_path = os.path.join(abs_path, relative_path) 
+    book = get_book_text(book_path)
+
+    print(book)
 
 
 if __name__ == "__main__":
