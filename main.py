681b3b633e52fb9c26df57289f69a29199685cb4
arturogood17 <arturo_good17@hotmail.com>
2025-11-14 22:39:58 +0100

    change: Main now prints every char and its count

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ def main():
 
     words = count_words(book)
     print(f"Found {words} total words")
-
+    print()
+    chars = count_char(book)
+    for k, v in chars.items():
+        print(f"'{k}': {v}")
+    print()
 
 if __name__ == "__main__":
     main()
