94dfcb852fa0888950625f016d70367749c0e0fb
arturogood17 <arturo_good17@hotmail.com>
2025-11-14 22:40:18 +0100

    feature: Added function to count chars

--- stats.py
+++ stats.py
@@ -5,4 +5,13 @@ def get_book_text(filepath):
 def count_words(book):
     b = book.split()
     return len(b)
-      
+
+def count_char(book):
+    chars = {}
+    for l in book:
+        l = l.lower()
+        if l in chars:
+            chars[l] += 1
+        else:
+            chars[l] = 1
+    return chars
