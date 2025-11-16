c1b4572ff79ae2b184bb722aa82aadff8216721b
arturogood17 <arturo_good17@hotmail.com>
2025-11-16 17:42:58 +0100

    feature: Report feature almost complete
    -Now a report is printed to the console
    -The each key:value pair is appended to a list and sorted.

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ def main():
     relative_path = "books/frankenstein.txt"
     book_path = os.path.join(abs_path, relative_path) 
     book = get_book_text(book_path)
-
     words = count_words(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {relative_path}")
+    print("----------- Word Count ----------")
     print(f"Found {words} total words")
-    print()
+    print("--------- Character Count -------")
     chars = count_char(book)
+    report =  []
     for k, v in chars.items():
-        print(f"'{k}': {v}")
-    print()
+        report.append({"char": k, "num": v})        
+    
+    report.sort(reverse= True, key=sort_on)
+    
+    for i in range(len(report)):
+        key = report[i]["char"]
+        if key.isalpha():
+            c = report[i]["num"]
+            print(f"{key}: {c}")
+    print("============= END ===============")
+
 
 if __name__ == "__main__":
     main()
